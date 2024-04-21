ed6e8ff6908b949996464796f545484e0597879d
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-04-22 01:03:47 +0300

    Update Markov.h

---
+++ include/Markov.h
@@ -0,0 +1,18 @@
+#include<vector>
+#include<locale>
+#include<string>
+#include<fstream>
+#include<map>
+#include<deque>
+#include <iostream>
+class Markov
+{
+public:
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > statetab;
+    prefix firstPref;
+    prefix bufPref;
+    void CreateStatetab(std::vector<std::string> words, int NREF);
+    void OperationStatetab(std::string str);
+    std::string CreateText(int MAXGEN);
+};
